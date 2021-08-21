5446601042f7ca96b16fba4c09fe37d75eb3c790
Anujmgr <anujmgr777@gmail.com>
2021-08-21 17:39:10 +0545

    minor

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,33 +16,6 @@ $(".main-banner-carousel").owlCarousel({
   },
 });
 
-$(".on-sale-carousel").owlCarousel({
-  loop: true,
-  dots: true,
-  autoplay: true,
-  margin: 20,
-  autoplayTimeout: 9000,
-  animateIn: "fadeIn", // add this
-  animateOut: "fadeOut", // and this
-
-  responsive: {
-    0: {
-      items: 1,
-    },
-    576: {
-      items: 2,
-    },
-    768: {
-      items: 3,
-    },
-    992: {
-      items: 5,
-      autoplay: false,
-      loop: false,
-    },
-  },
-});
-
 $(".icons-menu-carousel").owlCarousel({
   loop: true,
   dots: true,
@@ -109,3 +82,85 @@ $(".year").append(new Date().getFullYear());
 
 var height = $("header").height();
 $(".main-banner").css({ height: "calc(80vh - " + height + "px)" });
+
+// Owl Carousel with Thumbnails
+var sync1 = $(".thumbnail-carousel-slider");
+var sync2 = $(".carousel-thumbs");
+var thumbnailItemClass = ".owl-item";
+
+var slides = sync1
+  .owlCarousel({
+    video: true,
+    startPosition: 12,
+    items: 1,
+    loop: false,
+    margin: 10,
+    autoplay: false,
+    autoplayTimeout: 6000,
+    autoplayHoverPause: false,
+    nav: true,
+    dots: false,
+    touchDrag: false,
+    mouseDrag: false,
+  })
+  .on("changed.owl.carousel", syncPosition);
+
+function syncPosition(el) {
+  $owl_slider = $(this).data("owl.carousel");
+  var loop = $owl_slider.options.loop;
+
+  if (loop) {
+    var count = el.item.count - 1;
+    var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+    if (current < 0) {
+      current = count;
+    }
+    if (current > count) {
+      current = 0;
+    }
+  } else {
+    var current = el.item.index;
+  }
+
+  var owl_thumbnail = sync2.data("owl.carousel");
+  var itemClass = "." + owl_thumbnail.options.itemClass;
+
+  var thumbnailCurrentItem = sync2
+    .find(itemClass)
+    .removeClass("synced")
+    .eq(current);
+
+  thumbnailCurrentItem.addClass("synced");
+
+  if (!thumbnailCurrentItem.hasClass("active")) {
+    var duration = 300;
+    sync2.trigger("to.owl.carousel", [current, duration, true]);
+  }
+}
+var thumbs = sync2
+  .owlCarousel({
+    startPosition: 12,
+    items: 4,
+    loop: false,
+    margin: 10,
+    autoplay: false,
+    nav: false,
+    dots: false,
+    onInitialized: function (e) {
+      var thumbnailCurrentItem = $(e.target)
+        .find(thumbnailItemClass)
+        .eq(this._current);
+      thumbnailCurrentItem.addClass("synced");
+    },
+  })
+  .on("click", thumbnailItemClass, function (e) {
+    e.preventDefault();
+    var duration = 300;
+    var itemIndex = $(e.target).parents(thumbnailItemClass).index();
+    sync1.trigger("to.owl.carousel", [itemIndex, duration, true]);
+  })
+  .on("changed.owl.carousel", function (el) {
+    var number = el.item.index;
+    $owl_slider = sync1.data("owl.carousel");
+    $owl_slider.to(number, 100, true);
+  });
